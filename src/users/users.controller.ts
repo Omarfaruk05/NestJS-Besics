86115f54533eb5986625636b79794d1d0984882f
Md. Omar Faruk <mdomarfaruk149518@gmail.com>
2024-09-14 11:07:30 +0600

    [Understand]controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  /*
+    GET /users
+    GET /users/:id
+    POST /users
+    PATCH /users/:id
+    DELETE /users/:id
+  */
+
+  @Get() //GET /users or /users?role=value
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [role];
+  }
+
+  @Get('interns')
+  findAllInterns() {
+    return { interns: 'interns' };
+  }
+
+  @Get(':id') //GET /user/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  Create(@Body() user: {}) {
+    return user;
+  }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
